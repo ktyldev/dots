3fb6a7b354df8e00e906861c820e6ebafcc70883
ktyl <me@ktyl.dev>
2022-04-19 20:13:51 +0100

    ajouter des scripts de traduction

--- .scripts/include.sh
+++ .scripts/include.sh
@@ -10,3 +10,4 @@ export PATH=$PATH:$SCRIPTS/img
 export PATH=$PATH:$SCRIPTS/screenshot
 export PATH=$PATH:$SCRIPTS/anime-matrix
 export PATH=$PATH:$SCRIPTS/bitwarden
+export PATH=$PATH:$SCRIPTS/lang
