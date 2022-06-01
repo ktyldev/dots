1b780f774c8bc45f8d792e292885c6c2198d0cf8
ktyl <me@ktyl.dev>
2022-06-01 19:09:19 +0100

    update camera scripts but not usefully

--- .scripts/cam/env.sh
+++ .scripts/cam/env.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 export CAM_SRC="/run/media/$USER/disk/"
-export CAM_BKP="/run/media/$USER/7ACE1422CE13D4EB/cam/"
+
+# TODO: this is actually garbage lol, when importing pictures we should sort them
+# by their metadata, not by todays date and CERTAINLY not in an env variable which could
+# be in a shell started days ago
+
+# a dated directory to store 
+today="`date +%Y`/`date +%m`/`date +%d`"
+export CAM_TODAY="$HOME/pic/cam/$today"
