8336ffdacf23e89582c2ad75063129997b820e7b
ktyl <me@ktyl.dev>
2022-07-19 23:50:37 +0100

    improve camera import script

--- .scripts/cam/env.sh
+++ .scripts/cam/env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export CAM_SRC="/run/media/$USER/disk/"
+export CAM_PIC_SRC="$CAM_SRC/DCIM/100MSDCF"
 
 # TODO: this is actually garbage lol, when importing pictures we should sort them
 # by their metadata, not by todays date and CERTAINLY not in an env variable which could
