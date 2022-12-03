e6f78341907b22c54dd7f86161c436ad62ffad27
ktyl <me@ktyl.dev>
2022-12-03 15:52:18 +0000

    reorder imports

--- .scripts/include.sh
+++ .scripts/include.sh
@@ -5,10 +5,11 @@
 source $SCRIPTS/cam/env.sh
 
 export PATH=$PATH:$SCRIPTS
-export PATH=$PATH:$SCRIPTS/cam
-export PATH=$PATH:$SCRIPTS/img
-export PATH=$PATH:$SCRIPTS/screenshot
 export PATH=$PATH:$SCRIPTS/anime-matrix
 export PATH=$PATH:$SCRIPTS/bitwarden
-export PATH=$PATH:$SCRIPTS/lang
 export PATH=$PATH:$SCRIPTS/bops
+export PATH=$PATH:$SCRIPTS/cam
+export PATH=$PATH:$SCRIPTS/img
+export PATH=$PATH:$SCRIPTS/kbd
+export PATH=$PATH:$SCRIPTS/lang
+export PATH=$PATH:$SCRIPTS/screenshot
